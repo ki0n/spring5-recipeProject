d3feff05dee56bf77598a5419872dce404c374d5
Kion <daniarkano@gmail.com>
2018-08-25 18:29:28 +0200

    Spring Data jpa queries examples

--- spring5-recipe-app-master/src/main/java/guru/springframework/controllers/IndexController.java
+++ spring5-recipe-app-master/src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,13 +1,35 @@
 package guru.springframework.controllers;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import guru.springframework.domain.Category;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repository.CategoryRepository;
+import guru.springframework.repository.UnitOfMeasureRepository;
+
 @Controller
 public class IndexController {
-	
-	@RequestMapping({"","/","/index"})
+
+	private CategoryRepository categoryRepository;
+	private UnitOfMeasureRepository unitOfMeasureRepository;
+
+	public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+		this.categoryRepository = categoryRepository;
+		this.unitOfMeasureRepository = unitOfMeasureRepository;
+	}
+
+	@RequestMapping({ "", "/", "/index" })
 	public String getIndexPage() {
+		
+		Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+		Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+		
+		System.out.println("Cat id is: "+ categoryOptional.get().getId());
+		System.out.println("UOM id is: "+ unitOfMeasureOptional.get().getId());
+		
 		return "index";
 	}
 
