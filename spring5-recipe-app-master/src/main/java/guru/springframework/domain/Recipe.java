1725ff8af838f055a8591592de430927cc46e2ad
Kion <daniarkano@gmail.com>
2018-08-25 17:36:56 +0200

    Set h2 db connection and create category recipe and Unit of measure repositories

--- spring5-recipe-app-master/src/main/java/guru/springframework/domain/Recipe.java
+++ spring5-recipe-app-master/src/main/java/guru/springframework/domain/Recipe.java
@@ -4,10 +4,15 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
@@ -27,15 +32,24 @@ public class Recipe {
 	private String direccion;
 	// todo add
 	// private Difficulty difficulty;
-	
-	@OneToMany(cascade=CascadeType.ALL, mappedBy="recipe")
+
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
 	private Set<Ingredient> ingredient;
-	
+
 	@Lob
 	private Byte[] image;
 
+	@Enumerated(value = EnumType.STRING)
+	private Difficulty difficulty;
+
 	@OneToOne(cascade = CascadeType.ALL)
 	private Notes notes;
+
+	@ManyToMany
+	@JoinTable(name = "recipi_category", 
+	joinColumns = @JoinColumn(name = "recipe_id"), 
+	inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories;
 
 	public Long getId() {
 		return id;
@@ -115,6 +129,30 @@ public class Recipe {
 
 	public void setNotes(Notes notes) {
 		this.notes = notes;
+	}
+
+	public Set<Ingredient> getIngredient() {
+		return ingredient;
+	}
+
+	public void setIngredient(Set<Ingredient> ingredient) {
+		this.ingredient = ingredient;
+	}
+
+	public Difficulty getDifficulty() {
+		return difficulty;
+	}
+
+	public void setDifficulty(Difficulty difficulty) {
+		this.difficulty = difficulty;
+	}
+
+	public Set<Category> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(Set<Category> categories) {
+		this.categories = categories;
 	}
 
 }
