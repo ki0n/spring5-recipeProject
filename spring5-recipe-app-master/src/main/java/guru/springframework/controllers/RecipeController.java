4b79710819220f8c40cd93fae2dc0e3266a1a686
Kion <daniarkano@gmail.com>
2018-09-13 20:09:12 +0200

    delete recipe implement

--- spring5-recipe-app-master/src/main/java/guru/springframework/controllers/RecipeController.java
+++ spring5-recipe-app-master/src/main/java/guru/springframework/controllers/RecipeController.java
@@ -2,6 +2,7 @@ package guru.springframework.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +23,7 @@ public class RecipeController {
 		this.recipeService = recipeService;
 	}
 
+	@GetMapping
 	@RequestMapping("/recipe/{id}/show")
 	public String showById(@PathVariable String id, Model model) {
 		model.addAttribute("recipe", recipeService.findById(new Long(id)));
@@ -29,6 +31,7 @@ public class RecipeController {
 		return "recipe/show";
 	}
 
+	@GetMapping
 	@RequestMapping("recipe/new")
 	public String newRecipe(Model model) {
 		model.addAttribute("recipe", new RecipeCommand());
@@ -36,6 +39,7 @@ public class RecipeController {
 		return "recipe/recipeform";
 	}
 
+	@GetMapping
 	@RequestMapping("recipe/{id}/update")
 	public String updateRecipe(@PathVariable String id, Model model) {
 		model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
@@ -48,5 +52,15 @@ public class RecipeController {
 		RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
 		return "redirect:/recipe/" + savedCommand.getId() + "/show";
+	}
+	
+	@GetMapping
+	@RequestMapping("recipe/{id}/delete")
+	public String deleteById(@PathVariable String id) {
+		log.debug("Deleting id: " + id);
+		
+		recipeService.deleteById(Long.valueOf(id));
+		
+		return "redirect:/";
 	}
 }
--- spring5-recipe-app-master/src/main/java/guru/springframework/services/RecipeServiceImpl.java
+++ spring5-recipe-app-master/src/main/java/guru/springframework/services/RecipeServiceImpl.java
@@ -68,5 +68,10 @@ public class RecipeServiceImpl implements RecipeService {
 		return recipeToRecipeCommand.convert(findById(l));
 	}
 
+	@Override
+	public void deleteById(Long idToDelete) {
+		recipeRepository.deleteById(idToDelete);
+	}
+
 }
 
